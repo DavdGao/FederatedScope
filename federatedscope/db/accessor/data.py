c5f55de32ecb6237ef5981880746a269eb8dd81a
pxc <xc_pan@foxmail.com>
2022-08-19 13:55:04 +0800

    add mysql accessor

--- federatedscope/db/accessor/data.py
+++ federatedscope/db/accessor/data.py
@@ -148,3 +148,72 @@ def protocol_to_pandas(schemapb, rowspb):
             i = i + 1
         rows.append(row)
     return pd.DataFrame(rows, columns=schema.names())
+
+
+def parse_schema(schema):
+    """
+    parse a schema string
+    Args:
+        schema (list): schema of the csv file, contains a list of attribute descriptions, see parse_attribute for details
+
+    Returns:
+        Wrapper of protocol buffer Schema
+    """
+    schemapb = datapb.Schema()
+    attrpbs = []
+    for attr in schema:
+        if 'name' not in attr:
+            raise ValueError("No name in attribute definition")
+        if 'type' not in attr:
+            raise ValueError("No type in attribute definition")
+        attrpbs.append(parse_attribute(attr))
+    schemapb.attributes.extend(attrpbs)
+    return Schema(schemapb)
+
+def parse_attribute(attr):
+    """
+    parse attribute description into protocol buffer Attribute
+    Args:
+        attr: a dictionary describing attribute information, format:
+        {
+            'name': attribute name,
+            'type': accessor type(int|float|string),
+            'sensitve': whether the attribute is sensitive(optional, default false),
+            'min': int value(optional),
+            'max': max value(optional),
+            'delta': value interval(optional)
+        }
+
+    Returns:
+        protocol buffer Attribute
+    """
+    # TODO: @xuchen, It seems like the attributes of pb class are related to the keys in the dict, can we build a smart translator？
+    attrpb = datapb.Attribute()
+    attrpb.name = attr['name']
+    if attr['type'].lower() == 'int':
+        attrpb.type = datapb.DataType.INT
+    elif attr['type'].lower() == 'float':
+        attrpb.type = datapb.DataType.FLOAT
+    else:
+        attrpb.type = datapb.DataType.STRING
+    if 'sensitive' in attr:
+        attrpb.sensitive = bool(attr['sensitive'])
+    if 'primary' in attr:
+        attrpb.primary = bool(attr['primary'])
+    # set has_range when schema gives min max and delta
+    if 'min' in attr and 'max' in attr and 'delta' in attr:
+        if attrpb.type == datapb.DataType.INT:
+            attrpb.min_value.i = int(attr['min'])
+            attrpb.max_value.i = int(attr['max'])
+            attrpb.delta.i = int(attr['delta'])
+            attrpb.has_range = True
+        elif attrpb.type == datapb.DataType.FLOAT:
+            attrpb.min_value.f = float(attr['min'])
+            attrpb.max_value.f = float(attr['max'])
+            attrpb.delta.f = float(attr['delta'])
+            attrpb.has_range = True
+        else:
+            attrpb.has_range = False
+    else:
+        attrpb.has_range = False
+    return attrpb
--- federatedscope/db/accessor/mysql_accessor.py
+++ federatedscope/db/accessor/mysql_accessor.py
@@ -1,45 +1,46 @@
 from federatedscope.db.accessor.basic_accessor import BasicAccessor
+import federatedscope.db.accessor.data as data
 
-import pymysql
+from sqlalchemy import create_engine
+import pandas as pd
 
 class MySqlAccessor(BasicAccessor):
-    def __init__(self, user, passwd, database):
+    def __init__(self, user, passwd, host, port, database, schemas):
+        """
+        connect to mysql
+
+        Args:
+            user (string): user of mysql
+            passwd (string): passwd of the user
+            host (string): hostname of mysql
+            port (int): port of mysql
+            database (string): database to connect
+            schemas (dic): { <table_name>: <table_schema>, ...}
+                            e.g.
+                            {
+                                'user': [
+                                    {'name':'id','type':'int','primary':True},
+                                    {'name':'age','type':'int','min':17,'max':81,'delta':1}
+                                ]
+                            }
+        """
         super(MySqlAccessor, self).__init__()
-        self.database = database
-        self.cursor = self.connect(user, passwd, database)
+        self.engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(user, passwd, host, port, database))
+        self.tables = {}
+        self.schemas = {}
+        self.connect(schemas)
 
-    def connect(self, user, passwd, database):
-        # For now, we only support local dbms
-        db = pymysql.connect(host='localhost',
-                             user=user,
-                             password=passwd,
-                             database=database)
-
-        cursor = db.cursor()
-        return cursor
+    # todo: support multiple schemas
+    def connect(self, schemas):
+        for name, schema in schemas.items():
+            self.schemas[name] = data.parse_schema(schema)
 
     def get_schema(self, table_name):
-        self.cursor.execute(f"describe {self.database}.{table_name};")
-        # Parse into pb
-        # TODO: @xuchen, do we need to parse it into pb?
-        return self.cursor.fetchall()
-        for attr in self.cursor.fetchall():
-            name, type, null, key, default, extra = attr
-            # parse into pb
-        return None
-
-
+        return self.schemas[table_name]
 
     def get_table(self, table_name):
-        self.cursor.execute("SELECT * FROM {table_name}")
-        # Actually we can read the data one by one
-        return self.cursor.fetchall()
-
+        df = pd.read_sql_query(f"SELECT * FROM {table_name}", self.engine)
+        return data.Table(table_name, self.schemas[table_name], df)
 
     def close(self):
         self.cursor.close()
-
-
-if __name__ == '__main__':
-    acc = MySqlAccessor('root', '123456', 'mysql')
-    acc.get_schema('user')
