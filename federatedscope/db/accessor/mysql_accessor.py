e3fca07b9163154ba98cdaadb712a07cd13de228
panxuchen <xc_pan@foxmail.com>
2022-08-24 18:06:30 +0800

    add client yaml for mysql

--- federatedscope/db/accessor/mysql_accessor.py
+++ federatedscope/db/accessor/mysql_accessor.py
@@ -5,42 +5,43 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 class MySqlAccessor(BasicAccessor):
-    def __init__(self, user, passwd, host, port, database, schemas):
+    def __init__(self, root, schema):
         """
         connect to mysql
 
         Args:
-            user (string): user of mysql
-            passwd (string): passwd of the user
-            host (string): hostname of mysql
-            port (int): port of mysql
-            database (string): database to connect
-            schemas (dic): { <table_name>: <table_schema>, ...}
+            root(string): "mysql+pymysql://{user}:{passwd}@{host}:{port}/{database}"
+            schema (list): [ {schema of a table}, ... ]
                             e.g.
-                            {
-                                'user': [
+                            [{
+                                'name': 'user',
+                                'schema': [
                                     {'name':'id','type':'int','primary':True},
                                     {'name':'age','type':'int','min':17,'max':81,'delta':1}
                                 ]
-                            }
+                            },...]
         """
         super(MySqlAccessor, self).__init__()
-        self.engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(user, passwd, host, port, database))
+        self.engine = create_engine(root)
         self.tables = {}
         self.schemas = {}
-        self.connect(schemas)
+        self.default_table = ""
+        self.connect(schema)
 
     # todo: support multiple schemas
-    def connect(self, schemas):
-        for name, schema in schemas.items():
-            self.schemas[name] = data.parse_schema(schema)
+    def connect(self, schema):
+        for table in schema:
+            if self.default_table == "":
+                self.default_table = table['name']
+            self.schemas[table['name']] = data.parse_schema(table['schema'])
 
     def get_schema(self, table_name):
         return self.schemas[table_name]
+    
+    def get_table(self):
+        df = pd.read_sql_query(f"SELECT * FROM {self.default_table}", self.engine)
+        return data.Table(self.default_table, self.schemas[self.default_table], df)
 
     def get_table(self, table_name):
         df = pd.read_sql_query(f"SELECT * FROM {table_name}", self.engine)
         return data.Table(table_name, self.schemas[table_name], df)
-
-    def close(self):
-        self.cursor.close()
